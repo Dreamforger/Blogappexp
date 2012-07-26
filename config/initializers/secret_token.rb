d8040d559bb9f1ccee77d36792c6f6fb4d1906ae
Dreamforger <matthew_gill@hotmail.co.uk>
2012-07-27 00:35:54 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsblog::Application.config.secret_token = 'c92a211ac68c1dcd9943470310b0a9284be0954e49eb640f9ac89096e3487360b72115269406e12e8c6d730b55ca6394e471647094bd35fcbfbf8e81045a9057'
